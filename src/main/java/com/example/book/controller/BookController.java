2ff28024f07390b3020d14aa939e3c510a017306
Killerbee <rongvang1912tc@gmail.com>
2023-06-01 23:35:48 +0700

    update book details

--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.book.controller;
 
 import com.example.book.domain.BookDetails;
 import com.example.book.entity.Category;
+import com.example.book.repository.BookRepository;
 import com.example.book.repository.CategoryRepository;
 import com.example.book.service.BookService;
 import com.example.book.service.CategoryService;
@@ -10,11 +11,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +25,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class BookController {
     private final CategoryRepository categoryRepository;
+    private final BookRepository bookRepository;
     private final UserService userService;
     private final BookService bookService;
     private final CategoryService categoryService;
@@ -56,4 +60,24 @@ public class BookController {
         model.addAttribute("books",filtered_books);
         return "admin/book_index";
     }
+
+    @RequestMapping(path = "/admin/book-details/{id}", method = RequestMethod.GET)
+    public String bookDetails(@PathVariable("id") Integer book_id, Model model){
+        userService.updateModel(model);
+        BookDetails book_details = bookService.getBookDetails(book_id);
+        // Convert book attributes in front-end
+        String book_category = book_details.getCategory().getName();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String published_date = book_details.getPublished_date().format(formatter);
+        String recommended_age = Integer.toString(book_details.getRecommended_age());
+        String page = Integer.toString(book_details.getPage());
+        model.addAttribute("book",book_details);
+        model.addAttribute("book_category",book_category);
+        model.addAttribute("published_date",published_date);
+        model.addAttribute("recommended_age",recommended_age);
+        model.addAttribute("page",page);
+        return "admin/book_details";
+    }
+
+
 }
